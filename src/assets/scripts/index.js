86d232e89b0a1faf48afe6effe8c6afabb2448b2
joshuamil <joshuamil@gmail.com>
2017-10-09 00:52:24 -0400

    Added initial content

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,47 +1,80 @@
 require('../styles/application.scss');
 
+import * as config from '../config/content.json';
+
+/* Start the Application */
 const start = () => {
-  /* Call rendering function */
-  renderHeader();
-  renderNavigation();
-  renderContent();
-  renderFooter();
+  // Get a router object
+  const route = router();
+  // Call rendering functions
+  renderHeader(route);
+  renderNavigation(route);
+  renderContent(route);
+  renderFooter(route);
+};
+
+
+/* Get Page Details */
+const router = () => {
+
+  const response = {};
+  const hash = window.location.hash;
+  response.name = (hash) ? hash.replace(/\#/g,'') : 'home';
+
+  // Get content object from config
+  response.content = config.pages.find((f) => {
+    return f.name === response.name
+  }) || config.pages[0];
+
   /* Render content when the page hash changes */
   if ("onhashchange" in window) {
     window.onhashchange = start;
   }
+  return response;
 };
+
 
 /* Render Header */
-const renderHeader = () => {
-  const content = require('../content/header.html');
+const renderHeader = (route) => {
+  const obj = route.content;
   const header = document.querySelector('header');
-  header.innerHTML = content;
+  const video = document.querySelector('.video-container');
+  let state = 'inactive';
+  if (obj.header) {
+    const content = require('../content/header.html');
+    state = 'active';
+    header.innerHTML = content;
+  }
+  header.className = '';
+  video.className = 'video-container';
+  header.classList.add(state);
+  video.classList.add(state);
 };
 
-const toggleHeader = (state) => {
-  const header = document.querySelector('body');
-  if (state && state.length > 0) {
-    header.className = '';
-    header.classList.add(state);
-  } else {
-    header.classList.toggle('active');
-  }
-};
 
 /* Render Navigation */
-const renderNavigation = () => {
+const renderNavigation = (route) => {
+  let li;
   const content = require('../content/navigation.html');
+  const ul = document.createElement('ul');
   const nav = document.querySelector('nav');
   nav.innerHTML = content;
-  activateNavigation();
+  config.pages.forEach( (item) => {
+    if (item.hasOwnProperty('displayInNav') && item.displayInNav) {
+      li = `<li><a href="#${item.name}">${item.title}</a></li>`;
+      ul.innerHTML += li;
+    }
+  });
+  nav.appendChild(ul);
+  activateNavigation(nav);
 };
 
+
 /* Activate Navigation Button */
-const activateNavigation = () => {
+const activateNavigation = (nav) => {
   const button = document.querySelector('button.menu');
-  const menu = document.querySelector('nav ul');
-  const logo = document.querySelector('nav .logo');
+  const menu = nav.querySelector('ul');
+  const logo = nav.querySelector('.logo');
   logo.addEventListener('click', () => {
     window.location.href = '#home';
   });
@@ -53,27 +86,34 @@ const activateNavigation = () => {
   });
 };
 
+
 /* Render Markdown Content into HTML */
-const renderContent = () => {
-  const hash = window.location.hash;
-  let file = (hash) ? hash.replace(/\#/g,'') : 'home';
-  const content = require(`../content/${file}.html`);
+const renderContent = (route) => {
+
+  // Get content object from config
+  const obj = route.content;
+
+  // Set document content
+  const content = require(`../content/${obj.file}`);
   const main = document.querySelector('main');
   main.innerHTML = content;
-  const state = (file === 'home') ? 'active' : 'inactive';
-  toggleHeader(state);
-  if (file === 'about') {
-    document.querySelector('nav').classList.add('black');
-  } else {
-    document.querySelector('nav').classList.remove('black');
-  }
+
+  // Set navigation state
+  const nav = document.querySelector('nav');
+  nav.className = '';
+  nav.classList.add(obj.navigation);
 };
 
+
 /* Render Footer Content */
-const renderFooter = () => {
-  const content = require('../content/footer.html');
-  const footer = document.querySelector('footer');
-  footer.innerHTML = content;
+const renderFooter = (route) => {
+  const obj = route.content;
+  if (obj.footer) {
+    const ul = document.createElement('ul');
+    const content = require('../content/footer.html');
+    const footer = document.querySelector('footer');
+    footer.innerHTML = content;
+  }
 };
 
 start();
